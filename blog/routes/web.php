3301678fe991ee613b83387a1ba98ee5d0fda570
Asibul <asibulhasan.rucse.18@gmail.com>
2020-09-24 01:57:48 +0600

    coppied but not working properly...

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('post', [App\Http\Controllers\PostController::class, 'create'])->name('post.create');
+Route::post('post', [App\Http\Controllers\PostController::class, 'store'])->name('post.store');
+Route::get('/posts', [App\Http\Controllers\PostController::class, 'index'])->name('posts');
+Route::get('/article/{post:slug}', [App\Http\Controllers\PostController::class, 'show'])->name('post.show');
+Route::post('/comment/store', [App\Http\Controllers\CommentController::class, 'store'])->name('comment.add');
+Route::post('/reply/store', [App\Http\Controllers\CommentController::class, 'replyStore'])->name('reply.add');
