2774ce40ad7b1e11b7c51806e40deca70a6bb6f7
Asibul <asibulhasan.rucse.18@gmail.com>
2020-10-18 21:14:04 +0600

    mail sent using mailable class

--- Email-notification-authorization/app/Http/Controllers/EmailController.php
+++ Email-notification-authorization/app/Http/Controllers/EmailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMe;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -14,15 +15,15 @@ class EmailController extends Controller
     public function store()
     {
         request()->validate(['email' => 'required|email']);
-        Mail::raw('It works', function ($message) {
-            $message->from('john@johndoe.com', 'John Doe');
-            $message->sender('john@johndoe.com', 'John Doe');
-            $message->to(request('email'), 'John Doe');
-            $message->cc('john@johndoe.com', 'John Doe');
-            $message->bcc('john@johndoe.com', 'John Doe');
-            $message->replyTo('john@johndoe.com', 'John Doe');
-            $message->subject('Hello');
-        });
-        return redirect('/email')->with('message','email sent');
+        // Mail::raw('It works', function ($message) {
+        //     $message->from('john@johndoe.com', 'John Doe');
+        //     $message->sender('john@johndoe.com', 'John Doe');
+        //     $message->to(request('email'), 'John Doe');
+        //     $message->cc('john@johndoe.com', 'John Doe');
+        //     $message->subject('Hello');
+        // });
+
+        Mail::to(request('email'))->send(new ContactMe('Asibul'));
+        return redirect('/email')->with('message', 'email sent');
     }
 }
